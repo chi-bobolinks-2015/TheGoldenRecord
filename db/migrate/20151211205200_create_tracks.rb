c6851349af0d9edfbb9b6ddcda161fbb604c639d
gaw1990 <georgewambold@gmail.com>
2015-12-11 15:07:00 -0600

    Add basic validation and associations

--- db/migrate/20151211205200_create_tracks.rb
+++ db/migrate/20151211205200_create_tracks.rb
@@ -1,12 +1,12 @@
 class CreateTracks < ActiveRecord::Migration
   def change
     create_table :tracks do |t|
-      t.string :title
+      t.string :title, null: false
       t.string :artist
       t.string :composer
       t.text :description
-      t.integer :category_id
-      t.string :url
+      t.integer :category_id, null: false
+      t.string :url, null: false 
       t.integer :length
 
       t.timestamps null: false
--- db/migrate/20151211205251_create_categories.rb
+++ db/migrate/20151211205251_create_categories.rb
@@ -1,8 +1,8 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-      t.string :name
-      t.string :img
+      t.string :name, null: false
+      t.string :img, null: false
 
       t.timestamps null: false
     end
