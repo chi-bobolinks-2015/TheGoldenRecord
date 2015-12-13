0c7d97e0435b203f6c4f9c1398302a19b5cfda04
Marcus Davis & Regina Compton <pair+marcus-a-davis.reginafcompton@devbootcamp.com>
2015-12-13 11:47:09 -0600

    Add draggable and droppable functionality

--- app/assets/javascripts/views/sidebar.js
+++ app/assets/javascripts/views/sidebar.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
+
   $(".draggableTrack").draggable({
     containment: "document",
     cursor: "move",
-    snap: ".cell"
+    snap: ".cell",
+    helper: "clone"
   });
+
+// Move this to another sensical folder.
+  $(".cell").droppable({
+    drop: function(event, ui) {
+      var draggable = ui.draggable.clone();
+      console.log("The track with " + draggable.attr("url") + "  ---- " + draggable.attr("id"));
+      sounds.push({src: draggable.attr("url"), id: draggable.attr("id"), name: draggable.html()})
+      // console.log(sounds);
+
+    }
+  });
+
 });
