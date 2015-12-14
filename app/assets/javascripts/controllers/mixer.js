3081824d9843d63069258950c6c38a955a802bed
HANA worku & Regina Compton <pair+haworku.reginafcompton@devbootcamp.com>
2015-12-14 13:59:23 -0600

    Update hover function

--- app/assets/javascripts/controllers/mixer.js
+++ app/assets/javascripts/controllers/mixer.js
@@ -24,7 +24,7 @@ function loadSound(mix, trackId, divId) {
     return Number($(div).attr("id")) === divId;
   });
 
-  $(targetDiv).append("<p>" + trackSource.name + "</p>")
+  // $(targetDiv).append("<p>" + trackSource.name + "</p>")
   $(targetDiv).attr("track-id", trackId)
 }
 
