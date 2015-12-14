784efe70b32a758b80898cec9be700f05c0840eb
Regina Compton <reginafcompton@gmail.com>
2015-12-14 09:57:37 -0600

    Add collapsible sidebar features

--- app/assets/javascripts/views/sidebar.js
+++ app/assets/javascripts/views/sidebar.js
@@ -1,9 +1,17 @@
 $(document).ready(function() {
 
+  $(".category-name").on("click", function () {
+    $(this).children().toggleClass("hidden");
+  });
+
   $(".draggableTrack").draggable({
     containment: ".container",
     cursor: "move",
     snap: ".cell",
     helper: "clone"
+  });
+
+  $(".collapsible").on("click", function(){
+
   });
 });
