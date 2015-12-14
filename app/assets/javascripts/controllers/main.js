aa84cb413761200cf9817ab3325716f0f9f0bb99
gaw1990 <georgewambold@gmail.com>
2015-12-14 09:36:52 -0600

    Add track working

--- app/assets/javascripts/controllers/main.js
+++ app/assets/javascripts/controllers/main.js
@@ -4,22 +4,27 @@ $(document).ready(function(event){
 
   // loadSound(activeMix, 5, 1)
   // loadSound(activeMix, 6, 2)
-  var sound = new Howl({
-  	urls: ['https://s3.amazonaws.com/the-golden-record/Birds%2C+Hyena%2C+Elephant.wav']
-  }).play();
+  // var sound = new Howl({
+  // 	urls: ['https://s3.amazonaws.com/the-golden-record/Birds%2C+Hyena%2C+Elephant.wav']
+  // }).play();
 
-  $(window).on("keyup", function(event) {
-  	if (event.keyCode == 38) {
-  		sound.pause();
-  	}
-  	else if (event.keyCode == 40) {
-  		sound.play();
-  	}
-  })
+  // $(window).on("keyup", function(event) {
+  // 	if (event.keyCode == 38) {
+  // 		sound.pause();
+  // 	}
+  // 	else if (event.keyCode == 40) {
+  // 		sound.play();
+  // 	}
+  // })
 
-  var sound2 = new Howl({
-  	urls: ['https://s3.amazonaws.com/the-golden-record/ReginaApp-+Swing.mp3']
-  }).play();
+  // var sound2 = new Howl({
+  // 	urls: ['https://s3.amazonaws.com/the-golden-record/ReginaApp-+Swing.mp3']
+  // }).play();
+var someMix = new Mixer
+someMix.addTrack({'url' : 'https://s3.amazonaws.com/the-golden-record/Birds%2C+Hyena%2C+Elephant.wav', 'divId' : 1})
+someMix.addTrack({'url' : 'https://s3.amazonaws.com/the-golden-record/ReginaApp-+Soul+Jazz.mp3', 'divId' : 0})
+someMix.addTrack({'url' : 'https://s3.amazonaws.com/the-golden-record/ReginaApp-+Swing.mp3', 'divId' : 2})
+console.log(someMix.mix) 
 
 })
 
--- app/assets/javascripts/models/mixer.js
+++ app/assets/javascripts/models/mixer.js
@@ -5,7 +5,11 @@ Mixer = function () {
 //Adds a track to the mix. Example args = {url: sound.aws.com, divId: 1}
 Mixer.prototype.addTrack = function (args) {
 	var newTrack = new Howl({
-		url : [args[:url]]
+		url : [args['url']]
 	}); 
-	this.mix[args[:divId]] = newTrack
+	this.mix[args['divId']] = newTrack
 }
+
+// var someMix = new Mixer
+// someMix.addTrack({url: 'https://s3.amazonaws.com/the-golden-record/Birds%2C+Hyena%2C+Elephant.wav', divId: 1})
+// console.log(someMix.mix) 
