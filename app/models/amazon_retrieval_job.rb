82eaaf60f43543b411e1ec5428ca918add1c22a1
Marcus Davis <email": null>
2015-12-15 11:20:32 -0600

    Update amazon retrieval to create Tracks from all folders

--- app/models/amazon_retrieval_job.rb
+++ app/models/amazon_retrieval_job.rb
@@ -5,23 +5,40 @@ class AmazonRetrievalJob < ActiveRecord::Base
     s3 = Aws::S3::Client.new(region: "us-east-1")
     bucket_contents = s3.list_objects(bucket: "the-golden-record").contents
 
+    earth_folder = []
+    music_folder = []
     nature_folder = []
+    space_folder = []
+
 
     bucket_contents.each do |object|
-      if thing.key.include?("Nature/") && thing.key != "Nature/"
+      if object.key.include?("Earth/") && object.key != "Earth/"
+        earth_folder << object
+      elsif object.key.include?("Music/") && object.key != "Music/"
+        music_folder << object
+      elsif object.key.include?("Nature/") && object.key != "Nature/"
         nature_folder << object
+      elsif object.key.include?("Space/") && object.key != "Space/"
+        space_folder << object
       end
     end
 
-    nature_folder.each do |track|
-      Track.create(title: track.key.split(".")[0..-2].join, url: "https://the-golden-record.s3.amazonaws.com/Nature/#{track.key}", category: "Nature")
+    earth_folder.each do |track|
+      Track.create(title: track.key.split(".")[0], url: "https://the-golden-record.s3.amazonaws.com/#{track.key}", category: "Earth")
     end
 
-    # bucket_contents.each do |track|
-    #   # Tracks need a category
-    #   Track.create(title: track.key.split(".")[0..-2].join, url: "https://the-golden-record.s3.amazonaws.com/#{track.key}")
-    #   # https://the-golden-record.s3.amazonaws.com/#{track.key}
-    # end
+    music_folder.each do |track|
+      Track.create(title: track.key.split(".")[0], url: "https://the-golden-record.s3.amazonaws.com/#{track.key}", category: "Music")
+    end
+
+    nature_folder.each do |track|
+      Track.create(title: track.key.split(".")[0], url: "https://the-golden-record.s3.amazonaws.com/#{track.key}", category: "Nature")
+    end
+
+    space_folder.each do |track|
+      Track.create(title: track.key.split(".")[0], url: "https://the-golden-record.s3.amazonaws.com/#{track.key}", category: "Space")
+    end
+
 
   end
 end
