1694edba23e4faf3b2f590a45c95d3d0609b86bd
Marcus Davis <email": null>
2015-12-15 09:42:47 -0600

    Add method to create new Tracks from database

--- app/models/amazon_retrieval_job.rb
+++ app/models/amazon_retrieval_job.rb
@@ -1,12 +1,13 @@
-class AmazonRetrievalJob
-  def self.get_amazon_stuff
+class AmazonRetrievalJob < ActiveRecord::Base
+  def self.get_amazon_tracks
     # Get objects from inside the the-golden-record bucket
     # Using objects' information, create and save Track objects and clean out old objects
     s3 = Aws::S3::Client.new(region: "us-east-1")
-    bucket = s3.list_objects(bucket: "the-golden-record")
+    bucket_contents = s3.list_objects(bucket: "the-golden-record").contents
 
-    bucket.contents.each do |track|
-
+    bucket_contents.each do |track|
+      Track.create(title: track.key.split(".")[0..2].join, url: "https://the-golden-record.s3.amazonaws.com/#{track.key}")
+      # https://the-golden-record.s3.amazonaws.com/#{track.key}
     end
 
   end
